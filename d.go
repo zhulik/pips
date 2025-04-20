65c640ecc85f88cf8010c3bb976e3c221f7439ea
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-04-20 17:02:45 +0200

    Add D

---
+++ d.go
@@ -0,0 +1,45 @@
+package pips
+
+type D[T any] interface {
+	Unpack() (*T, error)
+
+	Value() *T
+	Error() error
+}
+
+func AnyD[T any](value *T) D[any] {
+	v := any(value)
+	return NewD[any](&v)
+}
+
+func NewD[T any](value *T) D[T] {
+	return pd[T]{value, nil}
+}
+
+func ErrD[T any](err error) D[T] {
+	return pd[T]{nil, err}
+}
+
+func CastD[I any, O any](d D[I]) D[O] {
+	if d.Error() != nil {
+		return ErrD[O](d.Error())
+	}
+	return NewD(any(d.Value).(*O))
+}
+
+type pd[T any] struct {
+	value *T
+	error error
+}
+
+func (r pd[T]) Value() *T {
+	return r.value
+}
+
+func (r pd[T]) Error() error {
+	return r.error
+}
+
+func (r pd[T]) Unpack() (*T, error) {
+	return r.value, r.error
+}
