a236ab8b7474d50a810579d31968f9e2de35c84b
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-05-10 22:52:22 +0200

    Add benchmark

---
+++ pipeline_benchmark_test.go
@@ -0,0 +1,50 @@
+package pips_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/zhulik/pips"
+	"github.com/zhulik/pips/apply"
+)
+
+var noOpMap = apply.Map(func(_ context.Context, i int) (int, error) {
+	return i, nil
+})
+
+var noOpBatchMap = apply.Map(func(_ context.Context, i []int) ([]int, error) {
+	return i, nil
+})
+
+// BenchmarkPipelineWith3NoOpMappingSteps measures how many items can be processed
+// by a pipeline with 3 mapping steps that do nothing.
+func BenchmarkPipelineWith3NoOpMappingSteps(b *testing.B) {
+	input := make(chan pips.D[int])
+
+	// Create a pipeline with 3 no-op mapping steps
+	pipeline := pips.New[int, int]().
+		Then(noOpMap).
+		Then(apply.Batch[int](10)).
+		Then(apply.Rebatch[int](30)).
+		Then(noOpBatchMap).
+		Then(apply.Flatten[int]()).
+		Then(noOpMap)
+
+	out := pipeline.Run(context.Background(), input)
+
+	// Reset the timer to exclude setup time
+	b.ResetTimer()
+
+	go func() {
+		defer close(input)
+		for i := 0; i < b.N; i++ {
+			input <- pips.NewD(i)
+		}
+	}()
+
+	// Wait for the result
+	err := out.Wait(context.Background())
+	if err != nil {
+		b.Fatal(err)
+	}
+}
