b98c082be5d811603d98c3fc4d757a4977c0b5e7
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-05-15 23:34:02 +0200

    Reimplement each with map

--- apply/each.go
+++ apply/each.go
@@ -2,7 +2,6 @@ package apply
 
 import (
 	"context"
-	"reflect"
 
 	"github.com/zhulik/pips"
 )
@@ -11,23 +10,7 @@ type eacher[T any] func(context.Context, T) error
 
 // Each creates an each stage.
 func Each[I any](eacher eacher[I]) pips.Stage {
-	return func(ctx context.Context, input <-chan pips.D[any], output chan<- pips.D[any]) {
-		pips.MapToDChan(ctx, input, output, func(ctx context.Context, item any, out chan<- pips.D[any]) error {
-			var err error
-
-			if anys, ok := item.([]any); ok {
-				var x I
-				err = eacher(ctx, convertSlice[I](anys, reflect.TypeOf(x).Elem()))
-			} else {
-				err = eacher(ctx, item.(I))
-			}
-			if err != nil {
-				return err
-			}
-
-			out <- pips.NewD(item)
-
-			return nil
-		})
-	}
+	return Map(func(_ context.Context, item I) (I, error) {
+		return item, eacher(context.Background(), item)
+	})
 }
