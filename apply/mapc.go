3ee2fe196d0ae632ce011e92c2d5ef2a6d32cd79
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-05-20 01:58:28 +0200

    Fix MapC stage

--- apply/mapc.go
+++ apply/mapc.go
@@ -8,32 +8,46 @@ import (
 
 // MapC creates a concurrent map stage.
 func MapC[I any, O any](concurrency int, mapper mapper[I, O]) pips.Stage {
-	semaphore := make(chan any, concurrency)
+	return func(ctx context.Context, input <-chan pips.D[any], output chan<- pips.D[any]) {
+		semaphore := make(chan any, concurrency)
+		midChan := make(chan pips.D[chan pips.D[any]], concurrency)
 
-	return pips.New[I, O]().
-		Then(
-			Map(func(ctx context.Context, i I) (<-chan pips.D[O], error) {
-				ch := make(chan pips.D[O], 1)
+		go func() {
+			defer close(semaphore)
+			defer close(midChan)
+
+			pips.MapToDChan(ctx, input, midChan, func(ctx context.Context, item any, out chan<- pips.D[chan pips.D[any]]) error {
+				semaphore <- true
+
+				ch := make(chan pips.D[any])
 
 				go func() {
-					semaphore <- nil
 					defer func() { <-semaphore }()
 
-					ch <- pips.NewD(mapper(ctx, i))
+					ch <- pips.NewD[any](mapper(ctx, item.(I)))
+					close(ch)
 				}()
 
-				return ch, nil
-			}),
-		).
-		Then(
-			Map(func(ctx context.Context, f <-chan pips.D[O]) (O, error) {
-				select {
-				case <-ctx.Done():
-					var o O
-					return o, ctx.Err()
-				case d := <-f:
-					return d.Unpack()
+				out <- pips.NewD(ch)
+
+				return nil
+			})
+		}()
+
+		pips.MapToDChan(ctx, midChan, output, func(ctx context.Context, c chan pips.D[any], out chan<- pips.D[any]) error {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+
+			case d := <-c:
+				v, err := d.Unpack()
+				if err != nil {
+					return err
 				}
-			}),
-		).ToStage()
+
+				out <- pips.NewD(v)
+				return nil
+			}
+		})
+	}
 }
