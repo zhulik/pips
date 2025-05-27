5f45fe62be931fad0d6dc71eba777d58233e3d46
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-05-28 01:26:50 +0200

    Reimplement Zip with Map

--- apply/zip.go
+++ apply/zip.go
@@ -2,36 +2,17 @@ package apply
 
 import (
 	"context"
-	"reflect"
 
 	"github.com/zhulik/pips"
 )
 
 // Zip creates a zip stage.
 func Zip[I any, O any](zipper mapper[I, O]) pips.Stage {
-	return func(ctx context.Context, input <-chan pips.D[any], output chan<- pips.D[any]) {
-		pips.MapToDChan(ctx, input, output, func(ctx context.Context, item any, out chan<- pips.D[any]) error {
-			var res O
-			var err error
-
-			castItem, err := pips.TryCast[I](item)
-			if err != nil {
-				return err
-			}
-
-			if anys, ok := item.([]any); ok {
-				var x I
-				res, err = zipper(ctx, convertSlice[I](anys, reflect.TypeOf(x).Elem()))
-			} else {
-				res, err = zipper(ctx, castItem)
-			}
-			if err != nil {
-				return err
-			}
-
-			out <- pips.AnyD(pips.NewP(castItem, res))
-
-			return nil
-		})
-	}
+	return Map(func(ctx context.Context, i I) (pips.P[I, O], error) {
+		o, err := zipper(ctx, i)
+		if err != nil {
+			return nil, err
+		}
+		return pips.NewP(i, o), nil
+	})
 }
